a6c94fa2afdcb08d05a6ddf46c9706b12ec00794
NiharMandahas <niharmandahas@gmail.com>
2025-05-06 12:57:58 +0530

    works without internet

--- Distributed/src/main/java/controller/hosting.java
+++ Distributed/src/main/java/controller/hosting.java
@@ -1,13 +1,12 @@
-package contorller;
+package controller;
 
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/trial")
 public class hosting {
     @GetMapping
     public String showhtml(){
